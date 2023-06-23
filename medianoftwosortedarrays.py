a8ad8c9c898ceebb2da0bb61b540c9c84324014e
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-06-23 22:04:42 +0530

    #Day20 of #StriversSheetChallenge

---
+++ medianoftwosortedarrays.py
@@ -0,0 +1,14 @@
+# TOOO DIFFICULT MANNNN
+
+# # nums1 = [1, 2, 5]
+# # nums2 = [3, 4, 6]
+# A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# B = [1, 2, 3, 4]
+# l1 = len(A)
+# l2 = len(B)
+# print((l1+l2)//2)
+# half = ((l1+l2)//2)
+
+# # this might not be what i was expecting
+
+# # find median
