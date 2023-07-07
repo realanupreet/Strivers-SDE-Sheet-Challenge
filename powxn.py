187f805a018e14586463f95cdb6c02f6039bd09d
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-07-07 23:06:50 +0530

    #Day34 of #StriversSheetChallenge

---
+++ powxn.py
@@ -0,0 +1,23 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # res = 1
+        if x == 0:
+            return 0
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+
+        def powhelper(x, n):
+            if x == 0:
+                return 0
+            if n == 1:
+                return x
+            # print(f"for {x} {n}")
+            res = powhelper(x, n // 2)
+            res = res * res
+            return x * res if n % 2 else res
+
+        res = powhelper(x, abs(n))
+
+        return res if n >= 0 else 1 / res
