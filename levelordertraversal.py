70b7387136f76802c66e121484d13dac6b63b961
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-07-20 07:05:34 +0530

    #Day47 of #StriversSheetChallenge

---
+++ levelordertraversal.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        if root is None:
+            return res
+        queue = [root]
+
+        while queue:
+            l = len(queue)
+            cl = []
+            # print("queue",queue)
+            for _ in range(l):
+                ele = queue.pop(0)
+                cl.append(ele.val)
+                if ele.left:
+                    queue.append(ele.left)
+                if ele.right:
+                    queue.append(ele.right)
+            res.append(cl)
+        # print("res",res)
+        return res
