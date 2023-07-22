478cdc778e94201ecd8c7c6b19eb5a1ff2bc588f
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-07-22 10:09:41 +0530

    #Day49 of #StriversSheetChallenge

---
+++ maxwidthofbinarytree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def widthOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        width = 0
+        level = [(1, root)]
+        while level:
+            width = max(width, level[-1][0] - level[0][0] + 1)
+            level = [kid
+                    for number, node in level
+                    for kid in enumerate((node.left, node.right), 2 * number)
+                    if kid[1]]
+        return width
