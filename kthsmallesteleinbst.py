d346b3b93990d19c031fb9e4592368114937c5f8
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-07-23 16:47:19 +0530

    #Day50 of #StriversSheetChallenge

---
+++ kthsmallesteleinbst.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
